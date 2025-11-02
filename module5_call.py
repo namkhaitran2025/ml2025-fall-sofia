aadc16dbc8db7d9f3008fa14beb76b0aa4e2d608
namkhaitran2025 <namkhaitran1122@gmail.com>
2025-11-01 21:11:27 -0700

    Add files via upload

---
+++ module5_call.py
@@ -0,0 +1,6 @@
+from module5_mod import List
+
+number_list = List()
+number_list.setSize()
+number_list.setList()
+number_list.searchValue()
---
+++ module5_mod.py
@@ -0,0 +1,23 @@
+class List():
+   def __init__(self):
+     self.size = 0
+     self.number_list = []
+
+   def setSize(self):
+     self.size = input("Input the size of the number list: ")
+     
+   def setList(self):
+     for index in range(0, int(self.size)):
+       value = input("Enter the value at index " + str(index) + ": ") 
+       self.number_list.append(int(value))
+
+   def searchValue(self):
+      # print("The list is: ", self.number_list)
+      print("____________________________________________")
+      search_value = input("Enter the value to search: ")
+      found = -1
+      for index in range(0, int(self.size)):
+         if self.number_list[index] == int(search_value):
+            found = index
+            break
+      print("Index of the value is: " + str(found))
